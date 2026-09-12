29145d5681980edf11525f634d1bf432bb499992
Cody <khangpham.kgp@gmail.com>
2026-09-12 03:15:39 -0400

    Create user model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+
+    # Use email as the unique identifier for registration and login
+    email = models.EmailField(unique=True)
+
+    timezone = models.CharField(
+        max_length=50,
+        default='America/New_York'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    # Remove username field as it's not needed when using email for authentication
+    username = None
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
